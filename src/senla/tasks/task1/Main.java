7099af0eb4448c2d72313c2bfb14824a5ffc92f6
Kirill Kozlov <ks.kozlov24@gmail.com>
2021-01-16 14:44:08 +0300

    added the task1/Main.java

---
+++ src/senla/tasks/task1/Main.java
@@ -0,0 +1,38 @@
+package senla.tasks.task1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+
+        try {
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                boolean isEven = NumberInfo.isEven(number);
+                boolean isPrime = NumberInfo.isPrime(number);
+
+                if (isPrime && isEven) {
+                    System.out.println("The number is prime and even.");
+                }
+                else if (!isPrime && isEven) {
+                    System.out.println("The number is not prime, but even.");
+                }
+                else if (isPrime) {
+                    System.out.println("The number is prime, but not even.");
+                }
+                else {
+                    System.out.println("The number is not prime and not even.");
+                }
+            }
+            else {
+                System.out.println("It seems that you entered not an integer or not a number at all." +
+                        "\nOr maybe your number is too big or small.");
+            }
+        } catch (Exception e) {
+            System.out.println("Oops... Some error occurred.");
+        }
+
+    }
+}
