f283bb597535a08d6a1fa06e98f0b4cd6756dd07
Kirill Kozlov <ks.kozlov24@gmail.com>
2021-01-17 20:06:37 +0300

    added the Item.java

---
+++ src/senla/tasks/task6/Item.java
@@ -0,0 +1,25 @@
+package senla.tasks.task6;
+
+public class Item {
+    private String name;
+    private double weight;
+    private double price;
+
+    public Item(String name, double weight, double price) {
+        this.name = name;
+        this.weight = weight;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
